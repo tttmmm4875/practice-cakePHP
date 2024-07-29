1cf9996c942f49bf3cad1e0d93aed445dc185559
tttmmm4875 <t.makino4875@example.com>
2024-07-29 20:55:04 +0900

    *機能追加  - 記事の詳細を表示する機能を追加  - 記事を追加する機能を追加  - 記事を編集する機能を追加  - 記事を削除する機能を追加

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,11 +3,40 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+// Text クラス
+use Cake\Utility\Text;
+// EventInterface クラス
+use Cake\Event\EventInterface;
+// Validator クラスをインポートします。
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config):void
     {
+        parent::initialize($config);
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
